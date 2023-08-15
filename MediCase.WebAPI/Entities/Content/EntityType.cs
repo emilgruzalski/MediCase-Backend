bbaa776211ddaeb347728463d3d54571bdbac553
Emil Grużalski <emil.gruzalski@gmail.com>
2023-08-15 13:28:59 +0200

    Add content dbContext.
    Co-Authored-By: tmatenebris <79363317+tmatenebris@users.noreply.github.com>

---
+++ MediCase.WebAPI/Entities/Content/EntityType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MediCase.WebAPI.Entities.Content;
+
+public partial class EntityType
+{
+    public uint TypeId { get; set; }
+
+    public string TypeValue { get; set; } = null!;
+
+    public virtual ICollection<Entity> Entities { get; set; } = new List<Entity>();
+}
