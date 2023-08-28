060a550cf442b63fa86c272715a94d916519c3c9
Emil Grużalski <emil.gruzalski@gmail.com>
2023-08-28 10:06:30 +0200

    Add EntityTypes to seeder.

--- MediCase.WebAPI/Seeder.cs
+++ MediCase.WebAPI/Seeder.cs
@@ -24,6 +24,40 @@ namespace MediCase.WebAPI
             _dbAdminContext.Database.EnsureCreated();
             _dbContentContext.Database.EnsureCreated();
             _dbModeratorContext.Database.EnsureCreated();
+
+            if (!_dbContentContext.EntityTypes.Any()) 
+            {
+                _dbContentContext.EntityTypes.AddRange(
+                    new Entities.Content.EntityType { TypeId = 4, TypeValue = "content" },
+                    new Entities.Content.EntityType { TypeId = 6, TypeValue = "content_container" },
+                    new Entities.Content.EntityType { TypeId = 11, TypeValue = "glossary" },
+                    new Entities.Content.EntityType { TypeId = 12, TypeValue = "glossary_entry" },
+                    new Entities.Content.EntityType { TypeId = 3, TypeValue = "navigation" },
+                    new Entities.Content.EntityType { TypeId = 5, TypeValue = "question" },
+                    new Entities.Content.EntityType { TypeId = 15, TypeValue = "question_container" },
+                    new Entities.Content.EntityType { TypeId = 13, TypeValue = "test" },
+                    new Entities.Content.EntityType { TypeId = 14, TypeValue = "test_question" }
+                );
+
+                _dbContentContext.SaveChanges();
+            }
+
+            if (!_dbModeratorContext.EntityTypes.Any()) 
+            {
+                _dbModeratorContext.EntityTypes.AddRange(
+                    new Entities.Moderator.EntityType { TypeId = 4, TypeValue = "content" },
+                    new Entities.Moderator.EntityType { TypeId = 6, TypeValue = "content_container" },
+                    new Entities.Moderator.EntityType { TypeId = 11, TypeValue = "glossary" },
+                    new Entities.Moderator.EntityType { TypeId = 12, TypeValue = "glossary_entry" },
+                    new Entities.Moderator.EntityType { TypeId = 3, TypeValue = "navigation" },
+                    new Entities.Moderator.EntityType { TypeId = 5, TypeValue = "question" },
+                    new Entities.Moderator.EntityType { TypeId = 15, TypeValue = "question_container" },
+                    new Entities.Moderator.EntityType { TypeId = 13, TypeValue = "test" },
+                    new Entities.Moderator.EntityType { TypeId = 14, TypeValue = "test_question" }
+                );
+
+                _dbModeratorContext.SaveChanges();
+            }
         }
     }
 }
